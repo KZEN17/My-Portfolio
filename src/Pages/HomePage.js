a22a2899d72c8d09e4fe8df35b8d4cf798f50abd
KZEN17 <zack.kargeen@gmail.com>
2021-04-02 19:27:44 +0700

    button shadow added

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -48,37 +48,42 @@ function HomePage() {
 						}}
 					/>
 				</div>
-				<h1 className="hero-text">
-					Hi, I am
-					<span> Zlatko </span>
-				</h1>
-				<p className="h-sub-text">
-					I am a web and mobile app developer based in the Philippines,
-					specializing in building (and occasionally designing), exceptional,
-					high-quality websites, web apps and mobile apps. After graduating from
-					Colt Steele's Web Developer Bootcamp i started working as a freelancer
-					on Fiverr, Upwork and Freelancer where I worked on a variety of
-					interesting and meaningfull projects on daily basis.
-				</p>
-				<div className="icons">
-					<a href="https://www.twitter.com/Zlatko1412" className="icon-holder">
-						<FontAwesomeIcon icon={faTwitter} className="icon yt" />
-					</a>
-					<a href="https://github.com/KZEN17" className="icon-holder">
-						<FontAwesomeIcon icon={faGithub} className="icon gh" />
-					</a>
-					<a
-						href="https://www.facebook.com/zlatko.nikoloski1"
-						className="icon-holder"
-					>
-						<FontAwesomeIcon icon={faFacebook} className="icon fb" />
-					</a>
-					<a
-						href="https://www.linkedin.com/in/zlatko-nikoloski-33596461"
-						className="icon-holder"
-					>
-						<FontAwesomeIcon icon={faLinkedin} className="icon yt" />
-					</a>
+				<div>
+					<h1 className="hero-text">
+						Hi, I am
+						<span> Zlatko </span>
+					</h1>
+					<p className="h-sub-text">
+						I am a web and mobile app developer based in the Philippines,
+						specializing in building (and occasionally designing), exceptional,
+						high-quality websites, web apps and mobile apps. After graduating
+						from Colt Steele's Web Developer Bootcamp i started working as a
+						freelancer on Fiverr, Upwork and Freelancer where I worked on a
+						variety of interesting and meaningfull projects on daily basis.
+					</p>
+					<div className="icons">
+						<a
+							href="https://www.twitter.com/Zlatko1412"
+							className="icon-holder"
+						>
+							<FontAwesomeIcon icon={faTwitter} className="icon yt" />
+						</a>
+						<a href="https://github.com/KZEN17" className="icon-holder">
+							<FontAwesomeIcon icon={faGithub} className="icon gh" />
+						</a>
+						<a
+							href="https://www.facebook.com/zlatko.nikoloski1"
+							className="icon-holder"
+						>
+							<FontAwesomeIcon icon={faFacebook} className="icon fb" />
+						</a>
+						<a
+							href="https://www.linkedin.com/in/zlatko-nikoloski-33596461"
+							className="icon-holder"
+						>
+							<FontAwesomeIcon icon={faLinkedin} className="icon li" />
+						</a>
+					</div>
 				</div>
 				<div className="icons">
 					<button>
